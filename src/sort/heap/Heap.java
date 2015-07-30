6e121b3bbbfbc5dfee994eca85bab83be7a18867
gehui1981 <gehgeh@sina.com>
2015-07-30 14:07:40 +0800

    change

--- src/sort/heap/Heap.java
+++ src/sort/heap/Heap.java
@@ -6,14 +6,25 @@ import org.apache.commons.lang3.RandomUtils;
 
 public class Heap {
 
+	public static void main(String args[]) {
+		int testArr[] = new int[10];
+		for (int i = 0; i < 10; i++) {
+			testArr[i] = i + 1; // RandomUtils.nextInt(1, 10);
+		}
+
+		Heap heap = new Heap(testArr);
+		heap.sort();
+		
+	}
+
 	private int arr[] = null;
 
 	public Heap(int arr[]) {
 		this.arr = arr;
-		
-		log("Before buid max heap : " + Arrays.toString(arr));
+
+		log("Before buid max heap : " + this.toString());
 		buildMaxHeap();
-		log("After buid max heap : " + Arrays.toString(arr));
+		log("After buid max heap : " + this.toString());
 	}
 
 	public void buildMaxHeap() {
@@ -45,28 +56,40 @@ public class Heap {
 	}
 
 	private int left(int i) {
-		return 2 * i;
+		return 2 * i + 1;
+		// return (i << 1) + 1;
 	}
 
 	private int right(int i) {
-		return 2 * i + 1;
+		return 2 * i + 2;
+		// return (i << 1) + 2;
 	}
 
 	private int parent(int i) {
 		return i / 2;
+		// return (i - 1) >> 1;
 	}
-	
+
 	private void log(String s) {
 		System.out.println(s);
 	}
-	
-	public static void main(String args[]) {
-		int testArr[] = new int[10];
-		for (int i = 0; i < 10; i++) {
-			testArr[i] = i + 1; // RandomUtils.nextInt(1, 10);
+
+	public void sort() {
+		int length = arr.length;
+		for (int i = length - 1; i > 0; i--) {
+			int temp = arr[i];
+			arr[i] = arr[0];
+			arr[0] = temp;
+
+			ajustHeap(i, 0);
 		}
 		
-		Heap heap = new Heap(testArr);
+		log("After sort : " + this.toString());
+	}
+
+	@Override
+	public String toString() {
+		return Arrays.toString(arr);
 	}
 
 }
